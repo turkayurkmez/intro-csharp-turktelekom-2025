f430baae898ded5bc6ae91fbfcb5fb6f41b65444
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-29 13:49:35 +0300

    Update project structure and add initial C# program
    - Updated `.gitattributes` for line endings and diff settings.
    - Expanded `.gitignore` to include Visual Studio temporary files.
    - Created/modified `introCsharp.sln` to define solution structure.
    - Added `introCsharp.csproj` targeting .NET 9.0 with relevant settings.
    - Implemented `Program.cs` with a simple "Hello, World" example.

---
+++ introCsharp/introCsharp/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace introCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, Dünya");
+
+            /*
+             * primitive -> ilkel
+             * complex -> karmaşık
+             */
+
+            /*
+             * -sayısal
+             * -sözel
+             * -mantıksal
+             */
+
+            //sayısal
+            //tam sayılar
+            byte maxByte = 255;
+            sbyte maxSByte = 127;
+
+            short maxShort = 32767;
+            ushort maxUShort = 65535;
+
+            int defaultInteger = 565;
+            uint dUint = 6524;
+
+            long veryBigNumber = 2122121545445454;
+            ulong bigThanBigNumber = 454545455454;
+
+            //ondalıklı sayılar:
+            double pi = 3.14;
+            float fPi = 3.14f;
+            decimal dPi = 0.000000000000000000000000000000001M;
+
+            //Sözel
+            char letter = '*';
+            string companyName = "Türk Telekom";
+            String company2 = "Bişey";
+
+            Console.WriteLine(companyName);
+
+            
+
+
+
+        }
+    }
+}
