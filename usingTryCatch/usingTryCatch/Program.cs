128259287ea4e4ac65a281dee1671e55ea299461
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-29 15:21:45 +0300

    Add initial project setup for usingTryCatch app
    Created a new Visual Studio solution file (`usingTryCatch.sln`) and project file (`usingTryCatch.csproj`) for a console application. Added `Program.cs` to handle user input for integer division with exception handling for format errors and division by zero. Targeting .NET 9.0 with implicit usings and nullable reference types enabled.

---
+++ usingTryCatch/usingTryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace usingTryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Birinci sayıyı girin...");
+                int sayi1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("İkinci sayıyı girin...");
+                int sayi2 = Convert.ToInt32(Console.ReadLine());
+                int bolum = sayi1 / sayi2;
+
+                Console.WriteLine($"sonuç: {bolum}");
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Lütfen yalnızca sayısal değer giriniz!");
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Tam sayılar 0'a bölünemez!!!");
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Bir hata meydana geldi: {ex.Message} ");
+            }
+            finally
+            {
+                Console.WriteLine("Burası her zaman çalışır....");
+            }
+
+
+        }
+    }
+}
