fc7e49f01ea4d943545c84dd0e5f2ff4ea866d21
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-30 16:09:10 +0300

    Add C# console app with random number guessing game
    Introduce a new Visual Studio solution and project files for a C# console application. The application generates a random number and prompts the user to guess it, providing feedback on the guesses. The implementation includes multiple levels of guessing to enhance interactivity.

---
+++ introFunctions/introFunctions/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+ * 1. rastgele bir sayı tut.
+ * 2. kullanıcının sayıyı tahmin etmesini iste.
+ * 3. sayıyı karşılaştır ve yönlendir
+ * 4. Bildiyse bir sonraki level'a geç.
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+int getRandomNumber(int level)
+{
+    Random random = new Random();
+    return random.Next(0, level * 100);
+}
+
+int getSuggestionFromUser()
+{
+    Console.WriteLine("Lütfen bir sayı giriniz:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool isFounded(int suggested, int generated)
+{
+    if (suggested > generated)
+    {
+        Console.WriteLine("Aşağı");
+        return false;
+    }
+    else if (suggested < generated)
+    {
+        Console.WriteLine("Yukarı");
+        return false;
+    }
+    Console.WriteLine("Bildiniz!");
+    return true;
+}
+
+//int divide (int x, int y)
+//{
+//    return x / y;
+//}
+
+//void showMessage(string message)
+//{
+//    Console.WriteLine(message);
+//}
+
+
+//int output = divide(12, 3);
+
+//showMessage(output.ToString());
+
+int level = 1;
+while (level <= 5)
+{
+    int randomNumber = getRandomNumber(level);
+    bool isCompleted = false;
+    while (!isCompleted)
+    {
+        int suggest = getSuggestionFromUser();
+        isCompleted = isFounded(suggest, randomNumber); 
+    }
+    level++;
+    Console.WriteLine($"{level}. aşamaya geçtiniz... Şimdi 1 ile {level *100} arasında sayıyı bulunuz" );
+}
