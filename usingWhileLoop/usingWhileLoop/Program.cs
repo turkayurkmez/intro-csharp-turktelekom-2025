de4524e375551b92e5a276c7aa64422ded363ba9
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-30 10:34:36 +0300

    Add number guessing game and project configuration
    Created a new Visual Studio solution file (`usingWhileLoop.sln`) to define project settings for Debug and Release modes. Added a C# source file (`Program.cs`) implementing a number guessing game that provides user feedback on guesses. Also created a project file (`usingWhileLoop.csproj`) targeting .NET 9.0 with implicit usings and nullable reference types enabled.

---
+++ usingWhileLoop/usingWhileLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Sayı bulunana kadar oyun devam eder.*/
+
+Random random = new Random();
+int tutulanSayi = random.Next(0, 101);
+bool bulunduMu = false;
+while (!bulunduMu)
+{
+    Console.WriteLine("Sayıyı tahmin edin:");
+    int tahmin = Convert.ToInt32(Console.ReadLine());
+    if (tahmin > tutulanSayi)
+    {
+        Console.WriteLine("Aşağı");
+    }
+    else if (tahmin < tutulanSayi)
+    {
+        Console.WriteLine("Yukarı");
+    }
+    else {
+        Console.WriteLine("Bildiniz");
+        bulunduMu = true;
+    }
+}
+
