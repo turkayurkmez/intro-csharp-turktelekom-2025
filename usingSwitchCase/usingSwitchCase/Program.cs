a82e6954113249e6b87998258e70a925e002ea18
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-30 10:19:44 +0300

    Add initial project files for traffic light console app
    Created a new Visual Studio solution file (`usingSwitchCase.sln`) and project file (`usingSwitchCase.csproj`). Added a C# source file (`Program.cs`) that implements a console application to prompt for traffic light colors and check the current day of the week using conditional statements and a switch case.

---
+++ usingSwitchCase/usingSwitchCase/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Trafik ışığı ne renk (Kirmizi, Sari, Yesil)");
+string color = Console.ReadLine();
+
+//is operatörü ile bir nesnenin tipini denetleyebilirsiniz.
+//if (color is string)
+//{
+//    Console.WriteLine("color değişkeninin tipi string");
+//}
+
+
+string state = "";
+if (color == "Kirmizi")
+{
+    state = "Dur!";
+}
+else if (color == "Sari")
+{
+    state = "Dikkatli ol";
+}
+else if (color == "Yesil")
+{
+    state = "Geç";
+}
+else
+{
+    state = "Böyle bir renk yok!!";
+}
+
+    Console.WriteLine(state);
+
+switch (color)
+{
+    case "Kirmizi":
+        state = "Dur!";
+        break;
+    case "Sari":
+        state = "Dikkatli ol";
+        break;
+    case "Yesil":
+        state = "Geç";
+        break;
+    default:
+        state = "Böyle bir renk yok!!";
+        break;
+}
+
+Console.WriteLine(state);
+
+DateTime today = DateTime.Now;
+
+switch (today.DayOfWeek)
+{ 
+    case DayOfWeek.Monday: 
+    case DayOfWeek.Tuesday:        
+    case DayOfWeek.Wednesday:        
+    case DayOfWeek.Thursday:
+    case DayOfWeek.Friday:
+        Console.WriteLine("Hafta içi...");
+        break;
+    case DayOfWeek.Saturday:
+    case DayOfWeek.Sunday:
+        Console.WriteLine("Hafta sonu");    
+        break;
+    default:
+        break;
+}
