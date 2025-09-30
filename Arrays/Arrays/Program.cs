70dee74ade11ecf17db5e085f0038752c438fd3c
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-30 13:48:59 +0300

    Add random number generation and performance metrics
    Updated `Program.cs` with formatting improvements and added functionality to generate an array of random integers. Implemented logic to find the maximum and minimum values, including execution time measurements for both traditional loops and LINQ methods.
    
    Created a new Visual Studio solution file `forLoop.sln` and added `forLoop.csproj` with project configuration settings, including target framework and nullable reference types.

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -3,10 +3,10 @@ Console.WriteLine("Hello, World!");
 //Tüm elemanları biliyorsanız:
 string[] customers = { "Ali", "Türkay", "Cansu", "Zeynep" };
 int[] ages = new int[customers.Length];
-Console.WriteLine($"Toplam eleman sayısı: {customers.Length}\nİlk eleman: {customers[0]}\nSon eleman: {customers[customers.Length-1]}" );
+Console.WriteLine($"Toplam eleman sayısı: {customers.Length}\nİlk eleman: {customers[0]}\nSon eleman: {customers[customers.Length - 1]}");
 
 Console.WriteLine($"Yeni range operatörü. Sondaki eleman: {customers[^1]}");
-int start=0;
+int start = 0;
 int end = 2;
 
 Console.WriteLine($"İlk iki eleman: {string.Join(",", customers[start..end])}");
@@ -14,12 +14,12 @@ Console.WriteLine($"İlk iki eleman: {string.Join(",", customers[start..end])}")
 
 
 int index = 0;
-while (index< customers.Length)
+while (index < customers.Length)
 {
     Console.WriteLine($"{customers[index]} isimli müşterinin yaşını girin: ");
     int age = Convert.ToInt32(Console.ReadLine());
     ages[index++] = age;
-   
+
 }
 
 Console.WriteLine($"Girilen toplam yaş: {ages.Length} ");
@@ -32,6 +32,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int onlarBasamagi = number / 10;
 int birlerBasamagi = number % 10;
-Console.WriteLine(onlar[onlarBasamagi] +" "+ birler[birlerBasamagi]);
+Console.WriteLine(onlar[onlarBasamagi] + " " + birler[birlerBasamagi]);
 
 
---
+++ forLoop/forLoop/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+int[] numbers = new int[10];
+int index = 0;
+Random random = new Random();
+while (index < numbers.Length)
+{
+    int randomNumber = random.Next(-20, 100);
+    numbers[index++] = randomNumber;
+}
+
+//en büyük:
+var stopWatch = Stopwatch.StartNew();
+int enBuyuk = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    
+    if (numbers[i]> enBuyuk)
+    {
+        enBuyuk = numbers[i];
+    }
+}
+
+stopWatch.Stop();
+double forLoopBigOne = stopWatch.Elapsed.TotalMilliseconds;
+
+//en küöük:
+stopWatch.Restart();
+int enKucuk = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < enKucuk)
+    {
+        enKucuk = numbers[i];
+    }
+}
+
+stopWatch.Stop();
+
+long forLoopSmallOne = stopWatch.ElapsedMilliseconds;
+
+Console.WriteLine($"Tüm sayılar: {string.Join(", ",numbers)}");
+
+Console.WriteLine($"En büyük değer: {enBuyuk}");
+Console.WriteLine($"En küçük değer: {enKucuk}");
+
+stopWatch.Restart();
+Console.WriteLine($"En büyük değer - LINQ: {numbers.Max()}");
+stopWatch.Stop();
+
+long linqBigOne = stopWatch.ElapsedMilliseconds;
+
+Console.WriteLine($"En küçük değer: {numbers.Min()}");
+Console.WriteLine($"Ortalama değer: {numbers.Average()}");
+
+
+Console.WriteLine($"for ile büyük sayı: {forLoopBigOne} ms\nfor ile küçük: {forLoopSmallOne} ms\nlinq ile büyük {linqBigOne} ms");
