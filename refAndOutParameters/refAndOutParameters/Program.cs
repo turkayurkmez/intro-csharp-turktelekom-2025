6dc4253e7873fb303d9d6fc346df6b96d0a5e6ee
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-01 12:23:44 +0300

    Add initial project setup with ref and out parameters
    Created a new Visual Studio solution file (`refAndOutParameters.sln`) and a project file (`refAndOutParameters.csproj`) for a console application targeting .NET 9.0. Added a C# source file (`Program.cs`) demonstrating the use of `out` and `ref` parameters in method implementations.

---
+++ refAndOutParameters/refAndOutParameters/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void test(string message) //message bir input parametredir
+{
+    Console.WriteLine(message);
+}
+
+bool isInt(string value, out int number)
+{
+	try
+	{
+		number = Convert.ToInt32(value);
+		return true;
+	}
+	catch (Exception)
+	{
+		number = 0;
+		return false;
+	}
+}
+
+void normalProcess(int value)
+{
+	value *= 2;
+    Console.WriteLine($"normalProcess içinde value değeri: {value}");
+}
+
+void refProcess(ref int value)
+{
+    value *= 2;
+    Console.WriteLine($"refProcess içinde value değeri: {value}");
+}
+
+
+//out Kullanımı
+int outputVariable = 0;//boş tabak
+string testValue = "8";
+if (isInt(testValue, out outputVariable))
+{
+    Console.WriteLine($" {testValue.GetType().Name} tipindeki {testValue} değeri, tam sayıyya ({outputVariable.GetType().Name}) dönüştürüldü");
+}
+
+int number = 8;
+normalProcess(number);
+Console.WriteLine($"normalProcess metodunu çağırdıktan sonra number değeri: {number}");
+
+refProcess(ref number);
+Console.WriteLine($"refProcess metodunu çağırdıktan sonra number değeri: {number}");
+
