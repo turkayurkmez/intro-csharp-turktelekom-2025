4e5a628b9f9f208d25a54ee4b95c837f3673e685
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-01 16:02:26 +0300

    Add .NET project and ReportGenerator class
    Introduce a new Visual Studio solution and project files for a .NET application. Add a console application in `Program.cs` that demonstrates the usage of the `ReportGenerator` class. The `ReportGenerator` class is defined in `ReportGenerator.cs` with properties for output format and Excel file path, along with constructors for initialization. Comments provide context on immutability and class purpose.

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Constructor;
+
+Console.WriteLine("Hello, World!");
+ReportGenerator reportGenerator = new ReportGenerator("C:\\rapor.xlsx");
+Console.WriteLine(reportGenerator.OutputFormat);
+
+ReportGenerator reportGenerator1 = new ReportGenerator("C:\\test.xlsx");
+reportGenerator1.OutputFormat = "PDF";
+reportGenerator1.OutputFormat = "HTML";
+
+ReportGenerator reportGenerator2 = new ReportGenerator("test.xlsx", "HTML", "Professional");
+
+
+ReportGenerator reportGenerator3 = new ReportGenerator("a.xlsx") { OutputFormat = "PDF", ReportFormat = "Basic" };
+
---
+++ Constructor/Constructor/ReportGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class ReportGenerator
+    {
+        public string OutputFormat { get; set; }
+
+        public string ExcelFilePath { get; set; }
+
+
+        /*eğer bir class'ın tüm set özellerikeri "init" olarak tanımlıysa bu nesne immutable'dır!'*/
+        public string ReportFormat { get; init; }
+
+        //public ReportGenerator()
+        //{
+        //    OutputFormat = "PDF";
+        //}
+
+        public ReportGenerator(string excelFilePath)
+        {
+            ExcelFilePath = excelFilePath;
+        }
+
+        public ReportGenerator(string excelFilePath, string outputFormat, string reportFormat)
+        {
+            ExcelFilePath = excelFilePath;
+            OutputFormat = outputFormat;
+            ReportFormat = reportFormat;
+                
+        }
+
+        //Record icad edildi...
+    }
+}
