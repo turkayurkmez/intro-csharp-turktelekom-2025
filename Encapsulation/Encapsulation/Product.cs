33f9b138ec907a941612f044ca54323aba1cacda
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-01 15:31:05 +0300

    Add Product class and .NET 9.0 project structure
    Introduce a new Visual Studio solution and project files for a .NET application targeting .NET 9.0. Implement a `Product` class with encapsulated properties and methods for managing price and stock. Update `Program.cs` to demonstrate usage of the `Product` class, including user input for price and displaying stock availability.

---
+++ Encapsulation/Encapsulation/Product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Product
+    {
+        private decimal price;
+
+        public void SetPrice(decimal value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("fiyat değeri negatif olamaz");
+            }
+
+            this.price = value;
+        }
+
+        public decimal GetPrice()
+        {
+            return this.price;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(value);
+                }
+                this.name = value;
+            }
+        }
+
+        public string Category { get; set; }
+
+        //private bool inStock = false;
+        public bool InStock { get; private set; }
+
+        public int Stock { get; set; }
+
+        public void ChangeStock(int stock)
+        {
+            Stock = stock;
+            InStock = Stock > 0;
+
+        }
+        //private int stockCount;
+        //public int StockCount
+        //{
+        //    get
+        //    {
+        //        return stockCount;
+        //    }
+        //    set
+        //    {
+        //        stockCount = value;
+        //        inStock = true;
+        //    }
+        //}
+
+
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Encapsulation;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Fiyat giriniz:");
+decimal price = Convert.ToDecimal(Console.ReadLine());
+
+Product book = new Product();
+
+book.SetPrice(price);
+Console.WriteLine($"Ürün fiyatı, {book.GetPrice()} olarak atandı");
+book.Name = "Albert Camus";
+book.Category = "Yayınlar";
+
+Console.WriteLine($"Ürün adı: {book.Name}, kategorisi: {book.Category}");
+
+
+Console.WriteLine(book.InStock);
+book.ChangeStock(100);
+Console.WriteLine(book.InStock);
