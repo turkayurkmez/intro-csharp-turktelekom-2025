4882987d28e58a7a9ce41f20d3f82d311d52bb44
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-01 14:05:39 +0300

    Add initial project structure and division functionality
    Created a new Visual Studio solution and project files.
    Added a `BolmeSonucu` class to hold division results.
    Updated `Program.cs` to demonstrate division methods using
    out parameters, a result object, tuples, and tuple
    deconstruction. Configured the project for .NET 9.0 with
    nullable reference types and implicit usings.

---
+++ TupleAndDeconstruction/TupleAndDeconstruction/BolmeSonucu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleAndDeconstruction
+{
+    public class BolmeSonucu
+    {
+        public int Sonuc { get; set; }
+        public int Kalan { get; set; }
+    }
+}
+
---
+++ TupleAndDeconstruction/TupleAndDeconstruction/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TupleAndDeconstruction;
+
+Console.WriteLine("Hello, World!");
+/*
+ * Problem. Bir fonksiyon olsun. İki sayının birbirlerine bölünce çıkan sonucu ve kalan bilgisini döndürsün
+ */
+
+int bolme(int a,  int b, out int kalan)
+{
+    kalan = a % b;
+    return a / b;
+}
+
+int modulo = 0;
+int sonuc = bolme(15, 7, out modulo);
+Console.WriteLine($"out parametresi ile bölme sonucu: {sonuc}, kalan:{modulo}");
+
+//Out parametresi yerine başka ne olabilir?
+BolmeSonucu bolmeIslemi(int a, int b)
+{
+    BolmeSonucu bolmeSonucu = new BolmeSonucu();
+    bolmeSonucu.Sonuc = a / b;
+    bolmeSonucu.Kalan = a % b;
+
+    return bolmeSonucu;
+
+}
+
+var sonucNesne = bolmeIslemi(15, 7);
+Console.WriteLine($"obje dönen bölme işlemi sonucu: {sonucNesne.Sonuc}, kalan ise {sonucNesne.Kalan} ");
+
+Tuple<int,int> tupleileBolme(int a, int b)
+{
+    return Tuple.Create(a / b, a % b);
+}
+
+var tupleSonuc = tupleileBolme(15, 7);
+Console.WriteLine($"Tuple ile Bölme işlemi sonucu: {tupleSonuc.Item1}, kalan ise {tupleSonuc.Item2} ");
+
+(int sonuc, int kalan) decoBolme(int a, int b)
+{
+    return (a / b, a % b);
+}
+
+(int sonucDegeri, int kalanDegeri) = decoBolme(15, 7);
+
+Console.WriteLine($"Tuple Decomposition ile Bölme işlemi sonucu: {sonucDegeri}, kalan ise {kalanDegeri} ");
+
