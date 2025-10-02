e87bc4ea5a37d088189daa272ade7323f6c6f3e8
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-02 14:45:54 +0300

    Add initial project structure and basic functionality
    Created a new Visual Studio solution and project files.
    Introduced a class `Oyuncu` with an interface `IKiyafet` and
    abstract class `UstGiyim`, along with derived classes.
    Added a simple console application in `Program.cs` that
    outputs "Hello, World!".

---
+++ LooselyCoupled/LooselyCoupled/Oyuncu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooselyCoupled
+{
+    public class Oyuncu
+    {
+
+        public void Giy(IKiyafet kiyafet)
+        {
+            
+        }
+    }
+
+    public interface IKiyafet
+    {
+
+    }
+
+    public abstract class UstGiyim : IKiyafet
+    {
+
+    }
+    public class Kazak : UstGiyim
+    {
+
+    }
+    public class KirmiziKazak : Kazak
+    {
+
+    }
+}
