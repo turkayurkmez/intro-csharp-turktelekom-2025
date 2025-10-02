37b1b9790a3b7505baa078c87d73d8660d96fa4d
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-02 12:28:22 +0300

    Add .NET 9.0 solution with document printing functionality
    Introduced a new Visual Studio solution and project for a .NET application. Added an `IPrintable` interface and an abstract `Document` class with derived classes: `ExcelDocument`, `WordDocument`, and `PDFDocument`. Implemented `Load`, `Save`, and `Print` methods. Added a `DocumentPrinter` class for printing documents. Updated `Program.cs` to demonstrate usage.

---
+++ AbstractVEInterface/AbstractVEInterface/Document.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractVEInterface
+{
+
+    public interface IPrintable
+    {
+        void Print();
+    }
+    public abstract class Document
+    {
+        public abstract void Save();
+        public abstract void Load();
+
+        //Patronlar; PDF dosyalarının çıktısı alınmayacak dediğine göre; Print fonksiyonu abstract sınıfın bir üyesi olamaz
+        //public abstract void Print();
+
+        public void Copy(string from, string to)
+        {
+            Console.WriteLine($"{from} adresinden {to} adresine kopyalandı!");
+        }
+      
+
+    }
+
+    public class ExcelDocument : Document, IPrintable
+    {
+        public override void Load()
+        {
+            Console.WriteLine("Excel dosyası açılıyor");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Excel dosyası çıktı alınıyor");
+
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Excel dosyası kaydediliyor.");
+
+        }
+    }
+
+    public class WordDocument : Document, IPrintable
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("WORD dosyası çıktı alınıyor");
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PDFDocument : Document
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+     
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DocumentPrinter
+    {
+        public void Print(IPrintable document)
+        {
+            document.Print();
+        }
+    }
+}
---
+++ AbstractVEInterface/AbstractVEInterface/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AbstractVEInterface;
+
+Console.WriteLine("Hello, World!");
+
+PDFDocument pdf = new PDFDocument();
+ExcelDocument excelDocument = new ExcelDocument();
+WordDocument wordDocument = new WordDocument();
+
+DocumentPrinter documentPrinter = new DocumentPrinter();
+//documentPrinter.Print(pdf);
+documentPrinter.Print(excelDocument);
+documentPrinter.Print(wordDocument);
+
