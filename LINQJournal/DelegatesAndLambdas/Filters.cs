80a3622f894749cbc2e1da10169f2e3437fe6849
türkay ürkmez <turkay.urkmez@outlook.com>
2025-10-03 12:27:39 +0300

    Add DelegatesAndLambdas project and filtering functionality
    - Introduced a new project "DelegatesAndLambdas" in the solution.
    - Created project file with SDK, output type, and target framework.
    - Added `Filters` class with a static `Filter` method for integer arrays.
    - Implemented a console application demonstrating various filtering techniques.

---
+++ LINQJournal/DelegatesAndLambdas/Filters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndLambdas
+{
+    public class Filters
+    {
+        //int tipinde parametre alıp bool döndüren, erkek adaylar için askerlik sorunu bulunmayan, seyahat edebilir fonksiyonlar aranıyor :)
+
+       // public delegate bool Criteria(int i);
+        //artık delegate yazmak yerine hazır generic built-in delegate tanımları kullanıyoruz
+        public static  int[] Filter(int[] numbers, Func<int,bool> criteria)
+        {
+          
+            List<int> result = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (criteria(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+    }
+}
---
+++ LINQJournal/DelegatesAndLambdas/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegatesAndLambdas;
+
+Console.WriteLine("Hello, World!");
+
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+bool isEven(int number)
+{
+    return number % 2==0;
+}
+
+
+
+void showNumbers(int[] numbers)
+{
+    Console.WriteLine(string.Join(",",numbers));
+}
+//Version 1.0:
+var version1 = Filters.Filter(numbers,isEven);
+var version2 = Filters.Filter(numbers, delegate (int num) { return num % 2 == 1; });
+var version3 = Filters.Filter(numbers, n => n >= 5);
+var version4 = numbers.Where(x => x % 3 == 0).ToArray();
+
+showNumbers(version4);
